7d61b6148c562efc84e7b93d1b933d1377c85f6b
balazs hideghety <balazs.hideghety@gmail.com>
2021-09-01 18:40:29 +0200

    Angular - update

--- SP.Test.Client/src/environments/environment.base.ts
+++ SP.Test.Client/src/environments/environment.base.ts
@@ -29,5 +29,5 @@ export const environment: ienvironment = {
  * import the following file, but please comment it out in production mode
  * because it will have performance impact when throw error
  *
- * import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ * import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
  */
